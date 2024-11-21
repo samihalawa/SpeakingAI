1eae84707bf27076d681c3b3d465bf3127ca9975
samihalawa <samihalawaster@gmail.com>
2024-11-21 02:11:59 +0100

    m

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './client/src'),
-      '@db': path.resolve(__dirname, './db')
+      '@': path.resolve(__dirname, './src')
     }
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
   }
-});
+})
