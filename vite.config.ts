ed5b7e037741640d9459b34166b06ed54f6aa4d2
Sami H <samihalawaster@gmail.com>
2025-01-11 05:42:44 +0100

    (Commit from mac2)

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      '@db': path.resolve(__dirname, './db')
     }
   },
   build: {
